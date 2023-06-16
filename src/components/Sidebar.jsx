829aaa679ea1a5923b4790591b23c54d0700d3dc
Matheus Leonardo <mtsleon@unicamp.br>
2023-06-16 12:52:51 -0300

    layout implementado (figma) + typescript + css-in-module + usando useState nos comentarios

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+
+import { PencilSimpleLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+import styles from './Sidebar.module.css'
+
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+
+            <img 
+            className={styles.cover} 
+            src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
+
+            <div className={styles.profile}>
+
+                <Avatar src="https://github.com/mtsleon.png" />
+
+                <strong>Matheus Leonardo</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilSimpleLine size="20" />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
